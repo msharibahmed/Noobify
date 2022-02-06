27c036c31fdd13e68644be44a40cb60daa55e449
Sharib <msharibahmed@gmail.com>
2022-02-07 01:58:41 +0530

    CHANGE: items list update after doing api pagination

--- app/src/main/java/com/example/myapplication/helpers/PlaylistAdapter.kt
+++ app/src/main/java/com/example/myapplication/helpers/PlaylistAdapter.kt
@@ -1,14 +1,16 @@
 package com.example.myapplication.helpers
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.PlaylistItemBinding
+import com.example.myapplication.models.PlaylistItemModel
 
-class PlaylistAdapter(private val context: Context, private val songsList: ArrayList<String>) :
+class PlaylistAdapter(private val context: Context) :
     RecyclerView.Adapter<PlaylistAdapter.PlaylistItemViewHolder>() {
-
+    private var myList = ArrayList<PlaylistItemModel.Short>()
     inner class PlaylistItemViewHolder(binding: PlaylistItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         var name = binding.textCreatorName
@@ -16,6 +18,8 @@ class PlaylistAdapter(private val context: Context, private val songsList: Array
         var contact = binding.btnCreatorContact
         var avatar = binding.imgCreatorAvatar
         var createdAt = binding.textCreatedAt
+        var songName = binding.textSongName
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaylistItemViewHolder {
@@ -29,12 +33,17 @@ class PlaylistAdapter(private val context: Context, private val songsList: Array
     }
 
     override fun onBindViewHolder(holder: PlaylistItemViewHolder, position: Int) {
-        holder.name.text = songsList[position]
+        holder.name.text = myList[position].title
     }
 
     override fun getItemCount(): Int {
-        return songsList.size
+        return myList.size
     }
 
+     @SuppressLint("NotifyDataSetChanged")
+     fun add(loadedData : PlaylistItemModel){
+        myList.addAll(loadedData.shorts)
+         notifyDataSetChanged()
+    }
 
 }
