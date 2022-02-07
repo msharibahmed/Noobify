23838f676898d0a5f0f5fe53b31b1fbd3da026eb
Sharib <msharibahmed@gmail.com>
2022-02-07 20:32:45 +0530

    ADD:  Date formatter, assign json values to viewholder

--- app/src/main/java/com/example/myapplication/helpers/PlaylistAdapter.kt
+++ app/src/main/java/com/example/myapplication/helpers/PlaylistAdapter.kt
@@ -2,11 +2,15 @@ package com.example.myapplication.helpers
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.text.format.DateUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.PlaylistItemBinding
 import com.example.myapplication.models.PlaylistItemModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PlaylistAdapter(private val context: Context) :
     RecyclerView.Adapter<PlaylistAdapter.PlaylistItemViewHolder>() {
@@ -34,7 +38,10 @@ class PlaylistAdapter(private val context: Context) :
     }
 
     override fun onBindViewHolder(holder: PlaylistItemViewHolder, position: Int) {
-        holder.name.text = myList[position].title
+        holder.name.text = "Anonymous"+ myList[position].creator.userID
+        holder.username.text = "@anonymous"+ myList[position].creator.userID
+        holder.createdAt.text = dateFormatter(myList[position].dateCreated)
+        holder.songName.text = myList[position].title
     }
 
     override fun getItemCount(): Int {
@@ -47,4 +54,12 @@ class PlaylistAdapter(private val context: Context) :
         notifyDataSetChanged()
     }
 
+}
+
+fun dateFormatter(dateStr:String):String{
+    val inputFormat: SimpleDateFormat = SimpleDateFormat("yyyy-MMM-dd")
+    var date : Date = inputFormat.parse(dateStr);
+    var niceDateStr :String = DateUtils.getRelativeTimeSpanString(date.time,Calendar.getInstance().timeInMillis, DateUtils.MINUTE_IN_MILLIS).toString()
+    Log.d("daaate","$niceDateStr")
+    return niceDateStr
 }
