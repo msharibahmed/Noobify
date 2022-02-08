99f404bf7749a997389c264204e263a925b23919
Sharib <msharibahmed@gmail.com>
2022-02-08 13:22:38 +0530

    ADD: RecyclerView item onclickListener functionality

--- app/src/main/java/com/example/myapplication/helpers/PlaylistAdapter.kt
+++ app/src/main/java/com/example/myapplication/helpers/PlaylistAdapter.kt
@@ -3,8 +3,8 @@ package com.example.myapplication.helpers
 import android.annotation.SuppressLint
 import android.content.Context
 import android.text.format.DateUtils
-import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.PlaylistItemBinding
@@ -12,12 +12,12 @@ import com.example.myapplication.models.PlaylistItemModel
 import java.text.SimpleDateFormat
 import java.util.*
 
-class PlaylistAdapter(private val context: Context) :
+class PlaylistAdapter(private val context: Context, private val listener: CustomItemClickListener) :
     RecyclerView.Adapter<PlaylistAdapter.PlaylistItemViewHolder>() {
     private var myList = ArrayList<PlaylistItemModel.Short>()
 
     inner class PlaylistItemViewHolder(binding: PlaylistItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+        RecyclerView.ViewHolder(binding.root), View.OnClickListener {
         var name = binding.textCreatorName
         var username = binding.textCreatorUsername
         var contact = binding.btnCreatorContact
@@ -25,7 +25,17 @@ class PlaylistAdapter(private val context: Context) :
         var createdAt = binding.textCreatedAt
         var songName = binding.textSongName
 
+
+        init {
+            binding.root.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            listener.onItemClickChangeMiniPlayer(myList[adapterPosition], adapterPosition)
+        }
+
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaylistItemViewHolder {
         return PlaylistItemViewHolder(
@@ -35,13 +45,17 @@ class PlaylistAdapter(private val context: Context) :
                 false
             )
         )
+
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: PlaylistItemViewHolder, position: Int) {
-        holder.name.text = "Anonymous"+ myList[position].creator.userID
-        holder.username.text = "@anonymous"+ myList[position].creator.userID
+
+        holder.name.text = "Anonymous" + myList[position].creator.userID
+        holder.username.text = "@anonymous" + myList[position].creator.userID
         holder.createdAt.text = dateFormatter(myList[position].dateCreated)
         holder.songName.text = myList[position].title
+
     }
 
     override fun getItemCount(): Int {
@@ -53,13 +67,25 @@ class PlaylistAdapter(private val context: Context) :
         myList.addAll(loadedData.shorts)
         notifyDataSetChanged()
     }
+    fun getNextItem(index: Int):PlaylistItemModel.Short{
+        return myList[index]
+    }
+
 
 }
 
-fun dateFormatter(dateStr:String):String{
+@SuppressLint("SimpleDateFormat")
+fun dateFormatter(dateStr: String): String {
     val inputFormat: SimpleDateFormat = SimpleDateFormat("yyyy-MMM-dd")
-    var date : Date = inputFormat.parse(dateStr);
-    var niceDateStr :String = DateUtils.getRelativeTimeSpanString(date.time,Calendar.getInstance().timeInMillis, DateUtils.MINUTE_IN_MILLIS).toString()
-    Log.d("daaate","$niceDateStr")
-    return niceDateStr
+    val date: Date? = inputFormat.parse(dateStr);
+    //Log.d("daaate","$niceDateStr")
+    return DateUtils.getRelativeTimeSpanString(
+        date!!.time,
+        Calendar.getInstance().timeInMillis,
+        DateUtils.MINUTE_IN_MILLIS
+    ).toString()
+}
+
+interface CustomItemClickListener {
+    fun onItemClickChangeMiniPlayer(playlistItemModel: PlaylistItemModel.Short, index: Int)
 }
